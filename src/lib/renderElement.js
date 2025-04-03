4976cbfd7b565b1f5afb8153be229be328b296d2
박소희 <sthgml@naver.com>
2025-04-04 08:01:30 +0900

    fix: updateElements (#3)
    * 배포환경 확인
    
    * base path 확인
    
    * base추가
    
    * redirect path확인
    
    * recirect path 실행
    
    * recirect path 수정
    
    * fix: redirect
    
    * fix: redirectPath
    
    * check
    
    * createVNode, normalizeVNode
    
    * fix: VNode
    
    * createElement
    
    * eventMangaer
    
    * console.log제거
    
    * Router
    
    * 이벤트 위임
    
    * event 위임
    
    * 야매 update

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -13,5 +13,6 @@ export function renderElement(vNode, container) {
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
   container.append(createdElements);
+  updateElement(container, createdElements, createdElements);
   setupEventListeners(container);
 }
